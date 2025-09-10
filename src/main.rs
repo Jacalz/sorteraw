4e19c05c027a2416ab54e3cf13ee09fd037384a8
Jacalz <jacalz@tutanota.com>
2025-09-10 11:06:55 +0200

    Add some initial boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    src: String,
+    dst: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    for entry in fs::read_dir(&args.src)? {
+        let path = entry?.path();
+        dbg!(path);
+    }
+    Ok(())
 }
