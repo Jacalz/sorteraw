e2e734a68675007d7d3242d2bd2f589f23e5f577
Jacalz <jacalz@tutanota.com>
2025-09-10 17:43:59 +0200

    Use HashSet of seen directories
    Cust number of syscalls down to less than 1/3 with my example.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::Parser;
+use std::collections::HashSet;
 use std::fs;
 use std::path::PathBuf;
 use time::{OffsetDateTime, format_description};
@@ -15,17 +16,20 @@ fn main() -> Result<()> {
     let args = Args::parse();
 
     let date_format = format_description::parse("[year]-[month]-[day]")?;
+    let mut seen_dirs = HashSet::new();
+
     for entry in fs::read_dir(&args.src)? {
         let entry = entry?;
         let path = entry.path();
 
         let time: OffsetDateTime = entry.metadata()?.modified()?.into();
         let dir = args.dst.join(time.format(&date_format)?);
-        if !dir.exists() {
-            fs::create_dir_all(&dir)?;
+        let new_location = dir.join(path.file_name().expect("file name not found"));
+        if !seen_dirs.contains(&dir) {
+            fs::create_dir(&dir)?;
+            seen_dirs.insert(dir);
         }
 
-        let new_location = dir.join(path.file_name().expect("file name not found"));
         fs::copy(&path, &new_location)?;
     }
     Ok(())
